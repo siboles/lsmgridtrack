7e7b26db2258352f0309136adecc902342dfa5f8
Scott Sibole <scott.sibole@gmail.com>
2017-11-08 14:10:36 -0700

    Mocked openpyxl

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['numpy', 'SimpleITK','vtk', 'vtk.util', 'yaml']
+MOCK_MODULES = ['numpy', 'SimpleITK','vtk', 'vtk.util', 'yaml', 'openpyxl']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
