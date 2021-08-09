f20362ca622bc4b82ae5a59a461615402d7e3e3a
siboles <scott.sibole@gmail.com>
2021-08-09 15:13:02 -0600

    Updated version number in setup.

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ class BinaryDistribution(Distribution):
         return False
 setup(
     name = 'lsmgridtrack',
-    version = '0.1',
+    version = '0.2',
     description = 'A Python module providing a framework for deformable image registration of 3D images from multiphoton laser scanning microscopy. It is aimed at a technique involving the photobleaching of a 3D grid onto the image and then observing this grid region in unloaded and loaded states.',
     packages = find_packages('.'),
     url = "https://github.com/siboles/lsmgridtrack",
@@ -17,7 +17,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: BSD 3-Clause',
 
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     python_requires='>=3.5',
