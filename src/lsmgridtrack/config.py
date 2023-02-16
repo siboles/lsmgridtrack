3607022c9ecb27047679c2b1640c10a728e4bc4b
scott.sibole@gmail.com <scott.sibole@gmail.com>
2023-02-15 19:45:52 -0700

    Changed docstring to follow pydantic style.

--- src/lsmgridtrack/config.py
+++ src/lsmgridtrack/config.py
@@ -7,107 +7,84 @@ import pydantic
 
 
 class RegMethodEnum(str, Enum):
+    """
+    Optimization method to employ in image registration.
+    """
+
     BFGS = "bfgs"
     CONJUGATE_GRADIENT = "conjugate_gradient"
     GRADIENT_DESCENT = "gradient_descent"
 
 
 class RegMetricEnum(str, Enum):
+    """
+    Metric to be minimized during registration.
+    """
+
     CORRELATION = "correlation"
     HISTOGRAM = "histogram"
 
 
 class RegSamplingEnum(str, Enum):
+    """
+    Image sampling strategy to be used when evaluating metric.
+    """
+
     RANDOM = "random"
     REGULAR = "regular"
 
 
 class ImageOptions(BaseModel):
-    """
-    :param dimension: Dimension of images.
-    :type dimension: int
-
-    :param spacing: Physical voxel size of images.
-    :type spacing: List[float]=[1.0, 1.0, 1.0], optional
-
-    :param resampling: Target physical voxel sizes for resampling.
-    :type resampling: List[float], optional
-
-    :param surface_direction: Normal vector along which to search for sample surface.
-    :type surface_direction: List[int], optional
-    """
-
     class Config:
         validate_assignment = True
 
     dimension: int = 3
+    """Dimension of images."""
     spacing: List[float]
+    """Physical voxel size of images."""
     resampling: Optional[List[float]]
+    """Target physical voxel sizes for resampling."""
     surface_direction: Optional[List[int]]
+    """Normal vector along which to search for sample surface."""
 
 
 class GridOptions(BaseModel):
-    """
-    :param origin: Voxel coordinates of grid origin
-    :type origin: List[int]
-
-    :param spacing: Voxel spacing of grid lines
-    :type spacing: List[int]
-
-    :param size: Grid size in each direction.
-    :type size: List[int]
-
-    :param upsampling: Factor to upsample grid by when outputting results.
-    :type upsampling: int, optional
-    """
-
     class Config:
         validate_assignment = True
 
     origin: List[int]
+    """Voxel coordinates of grid origin."""
     spacing: List[int]
+    """Voxel spacing of grid lines."""
     size: List[int]
-    upsampling: int = 1
+    """Grid size in each direction"""
+    upsampling: Optional[int] = 1
+    """Factor to upsample grid by when outputting results."""
 
 
 class RegistrationOptions(BaseModel):
-    """
-    :param method: Optimization method to employ for registration:
-    :type method: RegMethodEnum; enum_values=[BFGS,
-                                                  CONJUGATE_GRADIENT,
-                                                  GRADIENT_DESCENT]
-
-    :param metric: Metric to minimize.
-    :type metric: RegMetricEnum; enum_values=[CORRELATION, HISTOGRAM]
-
-    :param iterations: Maximum number of iterations to perform in optimization.
-    :type iterations: int=20,optional
-
-    :param sampling_fraction: Fraction of image to sample when evaluating metric.
-    :type sampling_fraction: float=0.05,optional
-
-    :param sampling_strategy: Volume sampling method to evaluate optimization metric.
-    :type sampling_strategy: RegSamplingEnum; enum_values=[RANDOM, REGULAR]
-
-    :param reference_landmarks: Fiducial voxel coordinates in reference image.
-    :type reference_landmarks: List[List[int]]
-
-    :param deformed landmarks: Fiducial voxel coordinates in deformed image.
-    :type deformed landmarks: List[List[int]]
-    """
-
     class Config:
         validate_assignment = True
 
     method: RegMethodEnum = RegMethodEnum.BFGS
+    """Optimization method to employ for registration"""
     metric: RegMetricEnum = RegMetricEnum.CORRELATION
+    """Metric to minimize."""
     iterations: int = 20
+    """Maximum number of iterations to perform in optimization."""
     sampling_fraction: float = 0.05
+    """Fraction of image to sample when evaluating metric."""
     sampling_strategy: RegSamplingEnum = RegSamplingEnum.RANDOM
+    """Volume sampling method to evaluate optimization metric."""
     shrink_levels: List[float]
+    """Factors to resample image by in a pyramidal registration.
+    If [1.0] then no pyramid is employed."""
     sigma_levels: List[float]
+    """Gaussian variance to smooth by at each pyramid level."""
     reference_landmarks: List[List[int]]
+    """Fiducial voxel coordinates in reference image."""
     deformed_landmarks: List[List[int]]
+    """Fiducial voxel coordinates in deformed image."""
 
 
 class Options(BaseModel):
