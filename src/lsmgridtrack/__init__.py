aa6b7165040cf6993dc559496d3fcadb1f1bf3a5
scott.sibole@gmail.com <scott.sibole@gmail.com>
2023-02-15 19:20:07 -0700

    Removed redundant imports since we are using find_package() now.

--- src/lsmgridtrack/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from .tracker import tracker
-from .tracker2d import tracker2d
-from . import utils
-from .fea import model
-from .orientation import orientation
