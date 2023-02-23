b8e2a7d44c3563151894ecd6591cf310cb15ecb4
scott.sibole@gmail.com <scott.sibole@gmail.com>
2023-02-23 12:13:05 -0700

    Moved source code.

--- src/lsmgridtrack/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(funcName)s() - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler()],
-)
